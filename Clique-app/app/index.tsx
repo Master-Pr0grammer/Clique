2c4c387fab1ebfe43b90bf3c121f94cf7e0a98ff
alittlerocket <zhang144444@gmail.com>
2024-11-09 17:56:13 -0500

    moved main to index. login path added

--- Clique-app/app/index.tsx
+++ Clique-app/app/index.tsx
@@ -1,7 +1,64 @@
-import { Login } from "@/components/Login/Login";
+import { SafeAreaView, StyleSheet, Text, TextInput, Image, View } from "react-native"; 
+import { styles } from "@/components/Styles/login_styles";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function Index() {
+export default function Index(){
+    const posts = get_posts();
     return (
-        <Login />
+        <ScrollView>
+            {posts.map((post, index) => (
+                <View key={index}>
+                    {display_post(post)}
+                </View>
+            ))}
+        </ScrollView>
     )
 }
+
+//TODO: Implement database call
+function get_posts(){
+    return [
+        {
+            'images':['../assets/images/test_posts/image1.png'], 
+            'title':'New York Pic',
+            'text':'Picture from NY trip'
+        },
+
+        {
+            'images':['../assets/images/test_posts/image2.png'], 
+            'title':'Mount Everest',
+            'text':'Tall mountain bro'
+        },
+
+        {
+            'images':['../assets/images/test_posts/image3.png'], 
+            'title':'RPI, am I right',
+            'text':'Picture of RPI building'
+        }
+    ]
+}
+
+function display_post(post_info: {
+    images: string[];
+    title: string;
+    text: string;
+}){
+    post_info.images.map((image_url, index) => (
+        console.log(image_url)
+    ))
+    return (
+        <View>
+            {post_info.images.map((image_url, index) => (
+                <Image key={index} source={{ uri: image_url }} />
+            ))}
+            <Text style={styles.title}>
+                {post_info.title}
+            </Text>
+            <Text>
+                {post_info.text}
+            </Text>
+        </View>
+    )
+}
+
+
--- Clique-app/app/login.tsx
+++ Clique-app/app/login.tsx
@@ -1,6 +1,6 @@
 import { Login } from "@/components/Login/Login";
 
-export default function Index() {
+export default function LoginPage() {
     return (
         <Login />
     )
