d68367cfbb32ab4081c18f3ac9bcede3c977d1a9
alittlerocket <zhang144444@gmail.com>
2024-11-09 16:57:50 -0500

    improving the login page

--- Clique-app/app/index.tsx
+++ Clique-app/app/index.tsx
@@ -1,86 +1,196 @@
-import { SafeAreaView, StyleSheet, Text, TextInput, Image, View } from "react-native"; 
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-export default function Index() {
+export default function Example() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#eBecf4" }}> 
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Image 
-                    source={require("../assets/images/Clique_Logo.png")}
-                    style={styles.headerImg}
-                    resizeMode="contain" // Maintain aspect ratio.
-                />
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#e8ecf4' }}>
+      <KeyboardAwareScrollView style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            alt="App Logo"
+            resizeMode="contain"
+            style={styles.headerImg}
+            source={require("../assets/images/Clique_Logo.png")} />
 
-                <Text style={styles.title}>Sign in</Text>
-                <Text style={styles.subtitle}></Text>
-            </View>
+          <Text style={styles.title}>
+            Sign in to <Text style={{ color: '#8B0000' }}>Clique</Text>
+          </Text>
+
+          <Text style={styles.subtitle}>
+            Become part of the community!
+          </Text>
         </View>
 
         <View style={styles.form}>
-            <View style={styles.input}>
-                <Text style={styles.inputLabel}>Email</Text>
-                <TextInput style={styles.inputControl}></TextInput>
-            </View>
-            <View style={styles.input}>
-                <Text style={styles.inputLabel}>Password</Text>
-                <TextInput style={styles.inputControl}></TextInput>
-            </View>
+          <View style={styles.input}>
+            <Text style={styles.inputLabel}>Email address</Text>
+
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              clearButtonMode="while-editing"
+              keyboardType="email-address"
+              onChangeText={email => setForm({ ...form, email })}
+              placeholder="john@example.com"
+              placeholderTextColor="#6b7280"
+              style={styles.inputControl}
+              value={form.email} />
+          </View>
+
+          <View style={styles.input}>
+            <Text style={styles.inputLabel}>Password</Text>
+
+            <TextInput
+              autoCorrect={false}
+              clearButtonMode="while-editing"
+              onChangeText={password => setForm({ ...form, password })}
+              placeholder="********"
+              placeholderTextColor="#6b7280"
+              style={styles.inputControl}
+              secureTextEntry={true}
+              value={form.password} />
+          </View>
+
+          <View style={styles.formAction}>
+            <TouchableOpacity
+              onPress={() => {
+                // TODO: handle onPress
+              }}>
+              <View style={styles.btn}>
+                <Text style={styles.btnText}>Sign in</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => {
+              // TODO: handle link
+            }}>
+            <Text style={styles.formLink}>Forgot password?</Text>
+          </TouchableOpacity>
         </View>
+      </KeyboardAwareScrollView>
+
+      <TouchableOpacity
+        onPress={() => {
+          // TODO: handle link
+        }}>
+        <Text style={styles.formFooter}>
+          Don't have an account?{' '}
+          <Text style={{ textDecorationLine: 'underline' }}>Sign up</Text>
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-        flex: 1,
-    },
-    header: {
-        marginVertical: 36,
-    },
-    headerImg: {
-        width: '100%',
-        height: undefined,
-        marginBottom: 20,
-    },
-    title: {
-        fontSize: 36,
-        fontWeight: '700',
-        color: '#1e1e1e',
-        textAlign: 'center',
-    },
-    subtitle: {
-        fontSize: 15,
-        fontWeight: '500',
-        color: '#929292',
-        textAlign: 'center', 
-    },
-    form: {
-        marginBottom: 24,
-        paddingHorizontal: 70,
-        flexGrow: 1,
-        flexShrink: 1,
-        flexBasis: 0,
-    },
-    input: {
-        marginBottom: 16,
-    },
-    inputLabel: {
-        fontSize: 17,
-        fontWeight: '600',
-        color: '#222',
-        marginBottom: 8,
-    },
-    inputControl: {
-        height: 50,
-        backgroundColor: '#fff',
-        paddingHorizontal: 16,
-        borderRadius: 12,
-        fontSize: 15,
-        fontWeight: '500',
-        color: '#222',
-        borderWidth: 1,
-        borderColor: '#C9D3DB',
-        borderStyle: 'solid',
-    },
-})
+  container: {
+    paddingVertical: 24,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  title: {
+    fontSize: 31,
+    fontWeight: '700',
+    color: '#1D2A32',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#929292',
+  },
+  /** Header */
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 36,
+  },
+  headerImg: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    marginBottom: 36,
+  },
+  /** Form */
+  form: {
+    marginBottom: 24,
+    paddingHorizontal: 24,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  formAction: {
+    marginTop: 4,
+    marginBottom: 16,
+  },
+  formLink: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#8B0000',
+    textAlign: 'center',
+  },
+  formFooter: {
+    paddingVertical: 24,
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#222',
+    textAlign: 'center',
+    letterSpacing: 0.15,
+  },
+  /** Input */
+  input: {
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#222',
+    marginBottom: 8,
+  },
+  inputControl: {
+    height: 50,
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#222',
+    borderWidth: 1,
+    borderColor: '#C9D3DB',
+    borderStyle: 'solid',
+  },
+  /** Button */
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: '#8B0000',
+    borderColor: '#8B0000',
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: '600',
+    color: '#fff',
+  },
+});
