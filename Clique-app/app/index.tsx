9815084461d57d46c2a8884c21b75b75f7760e11
alittlerocket <zhang144444@gmail.com>
2024-11-09 15:52:05 -0500

    index.tsx login page form

--- Clique-app/app/index.tsx
+++ Clique-app/app/index.tsx
@@ -1,18 +1,31 @@
-import { SafeAreaView, StyleSheet, Text, Image, View } from "react-native"; 
+import { SafeAreaView, StyleSheet, Text, TextInput, Image, View } from "react-native"; 
 
 export default function Index() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#8B0000" }}> 
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#eBecf4" }}> 
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image 
-                    source={require("../assets/images/react-logo.png")}
+                    source={require("../assets/images/Clique_Logo.png")}
                     style={styles.headerImg}
                     resizeMode="contain" // Maintain aspect ratio.
                 />
+
+                <Text style={styles.title}>Sign in</Text>
+                <Text style={styles.subtitle}></Text>
             </View>
         </View>
-        
+
+        <View style={styles.form}>
+            <View style={styles.input}>
+                <Text style={styles.inputLabel}>Email</Text>
+                <TextInput style={styles.inputControl}></TextInput>
+            </View>
+            <View style={styles.input}>
+                <Text style={styles.inputLabel}>Password</Text>
+                <TextInput style={styles.inputControl}></TextInput>
+            </View>
+        </View>
     </SafeAreaView>
   );
 }
@@ -28,13 +41,46 @@ const styles = StyleSheet.create({
     headerImg: {
         width: '100%',
         height: undefined,
-        aspectRatio: 1,
+        marginBottom: 20,
     },
     title: {
-        fontSize: 27,
+        fontSize: 36,
         fontWeight: '700',
         color: '#1e1e1e',
-        marginBottom: 6,
         textAlign: 'center',
-    }
+    },
+    subtitle: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#929292',
+        textAlign: 'center', 
+    },
+    form: {
+        marginBottom: 24,
+        paddingHorizontal: 70,
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 0,
+    },
+    input: {
+        marginBottom: 16,
+    },
+    inputLabel: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: '#222',
+        marginBottom: 8,
+    },
+    inputControl: {
+        height: 50,
+        backgroundColor: '#fff',
+        paddingHorizontal: 16,
+        borderRadius: 12,
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#222',
+        borderWidth: 1,
+        borderColor: '#C9D3DB',
+        borderStyle: 'solid',
+    },
 })
