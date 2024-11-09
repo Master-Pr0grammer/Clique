ec0d3fa1074d9e7ad36374ee983e4bd96ca2d422
alittlerocket <zhang144444@gmail.com>
2024-11-09 15:19:18 -0500

    remove hellowave; change index.tsx

--- Clique-app/app/index.tsx
+++ Clique-app/app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, Image, View } from "react-native"; 
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#8B0000" }}> 
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image 
+                    source={require("../assets/images/react-logo.png")}
+                    style={styles.headerImg}
+                />
+            </View>
+        </View>
+        
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flex: 1,
+    },
+    header: {
+        marginVertical: 36,
+    },
+    headerImg: {
+        flexBasis: '50%',
+        height: 'auto',
+        alignSelf: 'center',
+        marginBottom: 36,
+    },
+    title: {
+        fontSize: 27,
+        fontWeight: '700',
+        color: '#1e1e1e',
+        marginBottom: 6,
+        textAlign: 'center',
+    }
+})
