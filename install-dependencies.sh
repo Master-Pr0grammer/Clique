15e45df463e496102857a46873156734ad68f8c4
Ethan McCartney <147747206+Master-Pr0grammer@users.noreply.github.com>
2024-11-09 20:58:42 -0500

    add dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,3 +3,4 @@ npm install react-native-image-picker
 npm install react react-native expo
 npm install react-navigation react-native-gesture-handler
 npm install @react-navigation/native @react-navigation/stack
+npm install react-native-gallery-swiper
